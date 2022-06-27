14a71f3fe6eddf82e8aed6b2e759ac325af5080d
Zahkthar <zahkthar@gmail.com>
2022-06-27 13:59:29 +0200

    Movement des pièces

--- src/GameState.c
+++ src/GameState.c
@@ -14,7 +14,6 @@ typedef enum {
 typedef struct chessPiece {
     sprite sprite;
     chessTeam team;
-    SDL_bool isAlive;
     char pieceType; // 'R', 'K', etc...
 } chessPiece;
 
@@ -38,6 +37,10 @@ const int PIECES_COUNT = 32;
 int SQUARE_HEIGHT;
 int SQUARE_WIDTH;
 
+// Move the piece
+SDL_bool isASquareSelected;
+boardSquare *squareSelected;
+
 // Functions
 void initGameState(SDL_Window **window, SDL_Renderer **renderer) {
     (void)window;
@@ -54,104 +57,104 @@ void initGameState(SDL_Window **window, SDL_Renderer **renderer) {
     chessPieces = malloc(PIECES_COUNT * sizeof(chessPiece));
 
     // Les pièces blanches
-    chessPieces[0] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'R' }; // A1
+    chessPieces[0] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'R' }; // A1
     chessPieces[0].sprite.texture = LoadTexture(*renderer, "res/Images/whiteRook.png");
     SDL_QueryTexture(chessPieces[0].sprite.texture, NULL, NULL, &chessPieces[0].sprite.rect.w, &chessPieces[0].sprite.rect.h);
-    chessPieces[1] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'K' };
+    chessPieces[1] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'K' };
     chessPieces[1].sprite.texture = LoadTexture(*renderer, "res/Images/whiteKnight.png");
     SDL_QueryTexture(chessPieces[1].sprite.texture, NULL, NULL, &chessPieces[1].sprite.rect.w, &chessPieces[1].sprite.rect.h);
-    chessPieces[2] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'B' };
+    chessPieces[2] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'B' };
     chessPieces[2].sprite.texture = LoadTexture(*renderer, "res/Images/whiteBishop.png");
     SDL_QueryTexture(chessPieces[2].sprite.texture, NULL, NULL, &chessPieces[2].sprite.rect.w, &chessPieces[2].sprite.rect.h);
-    chessPieces[3] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'Q' };
+    chessPieces[3] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'Q' };
     chessPieces[3].sprite.texture = LoadTexture(*renderer, "res/Images/whiteQueen.png");
     SDL_QueryTexture(chessPieces[3].sprite.texture, NULL, NULL, &chessPieces[3].sprite.rect.w, &chessPieces[3].sprite.rect.h);
-    chessPieces[4] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'K' };
+    chessPieces[4] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'K' };
     chessPieces[4].sprite.texture = LoadTexture(*renderer, "res/Images/whiteKing.png");
     SDL_QueryTexture(chessPieces[4].sprite.texture, NULL, NULL, &chessPieces[4].sprite.rect.w, &chessPieces[4].sprite.rect.h);
-    chessPieces[5] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'B' };
+    chessPieces[5] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'B' };
     chessPieces[5].sprite.texture = LoadTexture(*renderer, "res/Images/whiteBishop.png");
     SDL_QueryTexture(chessPieces[5].sprite.texture, NULL, NULL, &chessPieces[5].sprite.rect.w, &chessPieces[5].sprite.rect.h);
-    chessPieces[6] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'K' };
+    chessPieces[6] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'K' };
     chessPieces[6].sprite.texture = LoadTexture(*renderer, "res/Images/whiteKnight.png");
     SDL_QueryTexture(chessPieces[6].sprite.texture, NULL, NULL, &chessPieces[6].sprite.rect.w, &chessPieces[6].sprite.rect.h);
-    chessPieces[7] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'R' };
+    chessPieces[7] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'R' };
     chessPieces[7].sprite.texture = LoadTexture(*renderer, "res/Images/whiteRook.png");
     SDL_QueryTexture(chessPieces[7].sprite.texture, NULL, NULL, &chessPieces[7].sprite.rect.w, &chessPieces[7].sprite.rect.h);
 
-    chessPieces[8] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'p' }; // A2
+    chessPieces[8] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'p' }; // A2
     chessPieces[8].sprite.texture = LoadTexture(*renderer, "res/Images/whitePawn.png");
     SDL_QueryTexture(chessPieces[8].sprite.texture, NULL, NULL, &chessPieces[8].sprite.rect.w, &chessPieces[8].sprite.rect.h);
-    chessPieces[9] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'p' };
+    chessPieces[9] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'p' };
     chessPieces[9].sprite.texture = LoadTexture(*renderer, "res/Images/whitePawn.png");
     SDL_QueryTexture(chessPieces[9].sprite.texture, NULL, NULL, &chessPieces[9].sprite.rect.w, &chessPieces[9].sprite.rect.h);
-    chessPieces[10] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'p' };
+    chessPieces[10] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'p' };
     chessPieces[10].sprite.texture = LoadTexture(*renderer, "res/Images/whitePawn.png");
     SDL_QueryTexture(chessPieces[10].sprite.texture, NULL, NULL, &chessPieces[10].sprite.rect.w, &chessPieces[10].sprite.rect.h);
-    chessPieces[11] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'p' };
+    chessPieces[11] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'p' };
     chessPieces[11].sprite.texture = LoadTexture(*renderer, "res/Images/whitePawn.png");
     SDL_QueryTexture(chessPieces[11].sprite.texture, NULL, NULL, &chessPieces[11].sprite.rect.w, &chessPieces[11].sprite.rect.h);
-    chessPieces[12] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'p' };
+    chessPieces[12] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'p' };
     chessPieces[12].sprite.texture = LoadTexture(*renderer, "res/Images/whitePawn.png");
     SDL_QueryTexture(chessPieces[12].sprite.texture, NULL, NULL, &chessPieces[12].sprite.rect.w, &chessPieces[12].sprite.rect.h);
-    chessPieces[13] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'p' };
+    chessPieces[13] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'p' };
     chessPieces[13].sprite.texture = LoadTexture(*renderer, "res/Images/whitePawn.png");
     SDL_QueryTexture(chessPieces[13].sprite.texture, NULL, NULL, &chessPieces[13].sprite.rect.w, &chessPieces[13].sprite.rect.h);
-    chessPieces[14] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'p' };
+    chessPieces[14] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'p' };
     chessPieces[14].sprite.texture = LoadTexture(*renderer, "res/Images/whitePawn.png");
     SDL_QueryTexture(chessPieces[14].sprite.texture, NULL, NULL, &chessPieces[14].sprite.rect.w, &chessPieces[14].sprite.rect.h);
-    chessPieces[15] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, SDL_TRUE, 'p' };
+    chessPieces[15] = (chessPiece) (chessPiece) { { NULL, { 0, 0, 0, 0 } }, WHITE_TEAM, 'p' };
     chessPieces[15].sprite.texture = LoadTexture(*renderer, "res/Images/whitePawn.png");
     SDL_QueryTexture(chessPieces[15].sprite.texture, NULL, NULL, &chessPieces[15].sprite.rect.w, &chessPieces[15].sprite.rect.h);
 
     // Les pièces noires
-    chessPieces[16] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'p' }; // A7
+    chessPieces[16] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'p' }; // A7
     chessPieces[16].sprite.texture = LoadTexture(*renderer, "res/Images/blackPawn.png");
     SDL_QueryTexture(chessPieces[16].sprite.texture, NULL, NULL, &chessPieces[16].sprite.rect.w, &chessPieces[16].sprite.rect.h);
-    chessPieces[17] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'p' };
+    chessPieces[17] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'p' };
     chessPieces[17].sprite.texture = LoadTexture(*renderer, "res/Images/blackPawn.png");
     SDL_QueryTexture(chessPieces[17].sprite.texture, NULL, NULL, &chessPieces[17].sprite.rect.w, &chessPieces[17].sprite.rect.h);
-    chessPieces[18] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'p' };
+    chessPieces[18] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'p' };
     chessPieces[18].sprite.texture = LoadTexture(*renderer, "res/Images/blackPawn.png");
     SDL_QueryTexture(chessPieces[18].sprite.texture, NULL, NULL, &chessPieces[18].sprite.rect.w, &chessPieces[18].sprite.rect.h);
-    chessPieces[19] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'p' };
+    chessPieces[19] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'p' };
     chessPieces[19].sprite.texture = LoadTexture(*renderer, "res/Images/blackPawn.png");
     SDL_QueryTexture(chessPieces[19].sprite.texture, NULL, NULL, &chessPieces[19].sprite.rect.w, &chessPieces[19].sprite.rect.h);
-    chessPieces[20] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'p' };
+    chessPieces[20] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'p' };
     chessPieces[20].sprite.texture = LoadTexture(*renderer, "res/Images/blackPawn.png");
     SDL_QueryTexture(chessPieces[20].sprite.texture, NULL, NULL, &chessPieces[20].sprite.rect.w, &chessPieces[20].sprite.rect.h);
-    chessPieces[21] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'p' };
+    chessPieces[21] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'p' };
     chessPieces[21].sprite.texture = LoadTexture(*renderer, "res/Images/blackPawn.png");
     SDL_QueryTexture(chessPieces[21].sprite.texture, NULL, NULL, &chessPieces[21].sprite.rect.w, &chessPieces[21].sprite.rect.h);
-    chessPieces[22] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'p' };
+    chessPieces[22] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'p' };
     chessPieces[22].sprite.texture = LoadTexture(*renderer, "res/Images/blackPawn.png");
     SDL_QueryTexture(chessPieces[22].sprite.texture, NULL, NULL, &chessPieces[22].sprite.rect.w, &chessPieces[22].sprite.rect.h);
-    chessPieces[23] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'p' };
+    chessPieces[23] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'p' };
     chessPieces[23].sprite.texture = LoadTexture(*renderer, "res/Images/blackPawn.png");
     SDL_QueryTexture(chessPieces[23].sprite.texture, NULL, NULL, &chessPieces[23].sprite.rect.w, &chessPieces[23].sprite.rect.h);
 
-    chessPieces[24] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'R' }; // A8
+    chessPieces[24] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'R' }; // A8
     chessPieces[24].sprite.texture = LoadTexture(*renderer, "res/Images/blackRook.png");
     SDL_QueryTexture(chessPieces[24].sprite.texture, NULL, NULL, &chessPieces[24].sprite.rect.w, &chessPieces[24].sprite.rect.h);
-    chessPieces[25] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'K' };
+    chessPieces[25] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'K' };
     chessPieces[25].sprite.texture = LoadTexture(*renderer, "res/Images/blackKnight.png");
     SDL_QueryTexture(chessPieces[25].sprite.texture, NULL, NULL, &chessPieces[25].sprite.rect.w, &chessPieces[25].sprite.rect.h);
-    chessPieces[26] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'B' };
+    chessPieces[26] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'B' };
     chessPieces[26].sprite.texture = LoadTexture(*renderer, "res/Images/blackBishop.png");
     SDL_QueryTexture(chessPieces[26].sprite.texture, NULL, NULL, &chessPieces[26].sprite.rect.w, &chessPieces[26].sprite.rect.h);
-    chessPieces[27] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'Q' };
+    chessPieces[27] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'Q' };
     chessPieces[27].sprite.texture = LoadTexture(*renderer, "res/Images/blackQueen.png");
     SDL_QueryTexture(chessPieces[27].sprite.texture, NULL, NULL, &chessPieces[27].sprite.rect.w, &chessPieces[27].sprite.rect.h);
-    chessPieces[28] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'K' };
+    chessPieces[28] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'K' };
     chessPieces[28].sprite.texture = LoadTexture(*renderer, "res/Images/blackKing.png");
     SDL_QueryTexture(chessPieces[28].sprite.texture, NULL, NULL, &chessPieces[28].sprite.rect.w, &chessPieces[28].sprite.rect.h);
-    chessPieces[29] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'B' };
+    chessPieces[29] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'B' };
     chessPieces[29].sprite.texture = LoadTexture(*renderer, "res/Images/blackBishop.png");
     SDL_QueryTexture(chessPieces[29].sprite.texture, NULL, NULL, &chessPieces[29].sprite.rect.w, &chessPieces[29].sprite.rect.h);
-    chessPieces[30] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'K' };
+    chessPieces[30] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'K' };
     chessPieces[30].sprite.texture = LoadTexture(*renderer, "res/Images/blackKnight.png");
     SDL_QueryTexture(chessPieces[30].sprite.texture, NULL, NULL, &chessPieces[30].sprite.rect.w, &chessPieces[30].sprite.rect.h);
-    chessPieces[31] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, SDL_TRUE, 'R' };
+    chessPieces[31] = (chessPiece) { { NULL, { 0, 0, 0, 0 } }, BLACK_TEAM, 'R' };
     chessPieces[31].sprite.texture = LoadTexture(*renderer, "res/Images/blackRook.png");
     SDL_QueryTexture(chessPieces[31].sprite.texture, NULL, NULL, &chessPieces[31].sprite.rect.w, &chessPieces[31].sprite.rect.h);
 
@@ -209,6 +212,8 @@ void initGameState(SDL_Window **window, SDL_Renderer **renderer) {
         }
     }
 
+    isASquareSelected = SDL_FALSE;
+    squareSelected = NULL;
 }
 
 void exitGameState() {
@@ -259,17 +264,40 @@ int gameStateUpdate(SDL_Window **window, SDL_Renderer **renderer, deltaTimeClock
                 indexRebuilt = (mouseClickRow * 8) + mouseClickCol; // Voir l'initialisation du plateau pour comprendre le calcul
 
                 if(indexRebuilt >= 0 && indexRebuilt <= 63) { // Le clic est bien dans la grille
+                    // Si on clique sur une pièce, enregistre son adresse
+                    // Si on clique sur une autre case alors qu'une case est séléctionnée, déplace la pièce
+
                     if(chessBoard[indexRebuilt].piece != NULL) {
-                        printf("You have clicked on %c at square %d-%d (id = %d)\n", chessBoard[indexRebuilt].piece->pieceType, mouseClickCol, mouseClickRow, indexRebuilt);
-                    } else {
-                        printf("You have clicked on the empty square %d-%d (id = %d)\n", mouseClickCol, mouseClickRow, indexRebuilt);
+                        isASquareSelected = SDL_TRUE;
+                        squareSelected = &chessBoard[indexRebuilt];
                     }
+                    
+                    if(isASquareSelected == SDL_TRUE) {
+                        if((chessBoard[indexRebuilt].col != squareSelected->col) || (chessBoard[indexRebuilt].row != squareSelected->row)) {
+                            // Déplace la pièce
+                            chessBoard[indexRebuilt].piece = squareSelected->piece;
+                            squareSelected->piece = NULL;
+                            squareSelected = NULL;
+                            isASquareSelected = SDL_FALSE;
+                        }
+                    }
+
+                    printf("clicked on square %d-%d (index = %d)\n", mouseClickCol, mouseClickRow, indexRebuilt);
+                    printf("isASquareSelected = %d\n", isASquareSelected);
                 }
 
                 break;
 
             default:
                 break;
+        }
+
+        
+        for(int i = 0; i < SQUARES_COUNT; ++i) {
+            if(chessBoard[i].piece != NULL) {
+                chessBoard[i].piece->sprite.rect.x = chessBoardSprite.rect.x + (chessBoard[i].col * SQUARE_WIDTH);
+                chessBoard[i].piece->sprite.rect.y = chessBoardSprite.rect.y + (chessBoard[i].row * SQUARE_HEIGHT);
+            }
         }
     }
 
